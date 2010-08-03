b2d2eb9d460738b7820347ef8407f6169a4a8629
Alexander Gladysh <agladysh@gmail.com>
2010-08-03 10:29:43 +0400

    filesystem: initial commit

---
+++ lua-aplicado/filesystem.lua
@@ -0,0 +1,194 @@
+--------------------------------------------------------------------------------
+-- filesystem.lua: basic code to work with files and directories
+--------------------------------------------------------------------------------
+
+-- TODO: Use debug.traceback() in do_atomic_op_with_file()?
+
+local lfs = require 'lfs'
+
+--------------------------------------------------------------------------------
+
+local arguments,
+      optional_arguments,
+      method_arguments,
+      eat_true
+      = import 'lua-nucleo/args.lua'
+      {
+        'arguments',
+        'optional_arguments',
+        'method_arguments',
+        'eat_true'
+      }
+
+local split_by_char
+      = import 'lua-nucleo/string.lua'
+      {
+        'split_by_char'
+      }
+
+--------------------------------------------------------------------------------
+
+local function find_all_files(path, regexp, dest)
+  dest = dest or {}
+
+  for filename in lfs.dir(path) do
+    if filename ~= "." and filename ~= ".." then
+      local filepath = path .. "/" .. filename
+      local attr = lfs.attributes(filepath)
+      if attr.mode == "directory" then
+        find_all_files(filepath, regexp, dest)
+      elseif filename:find(regexp) then
+        dest[#dest + 1] = filepath
+        -- print("found", filepath)
+      end
+    end
+  end
+
+  return dest
+end
+
+local write_file = function(filename, new_data)
+  arguments(
+      "string", filename,
+      "string", new_data
+    )
+
+  local file, err = io.open(filename, "w")
+  if not file then
+    return nil, err
+  end
+
+  file:write(new_data)
+  file:close()
+  file = nil
+
+  return true
+end
+
+local read_file = function(filename)
+  arguments(
+      "string", filename
+    )
+
+  local file, err = io.open(filename, "r")
+  if not file then
+    return nil, err
+  end
+
+  local data = file:read("*a")
+  file:close()
+  file = nil
+
+  return data
+end
+
+-- WARNING: Not atomic.
+-- Returns "skipped" if data was not changed
+local update_file = function(out_filename, new_data, force)
+  arguments(
+      "string", out_filename,
+      "string", new_data,
+      "boolean", force
+    )
+
+  local skip = false
+  if lfs.attributes(out_filename, "mode") then
+    local old_data, err = read_file(out_filename)
+    if not old_data then
+      return nil, err
+    end
+
+    skip = (old_data == new_data)
+    if not skip and not force then
+      return
+        nil,
+        "data is changed, refusing to override `" .. out_filename .. "'"
+    end
+  end
+
+  if not skip then
+    local res, err = write_file(out_filename, new_data)
+    if not res then
+      return nil, err
+    end
+
+    return true
+  end
+
+  return "skipped" -- Not changed
+end
+
+--------------------------------------------------------------------------------
+
+local create_path_to_file = function(filename)
+  arguments(
+      "string", filename
+    )
+
+  local path = false
+  local dirs = split_by_char(filename, "/")
+  for i = 1, #dirs - 1 do
+    path = path and (path .. "/" .. dirs[i]) or (dirs[i])
+    if path ~= "" and not lfs.attributes(path) then
+      local res, err = lfs.mkdir(path)
+      if not res then
+        return nil, "failed to create directory `" .. path .. "': " .. err
+      end
+    end
+  end
+
+  return true
+end
+
+--------------------------------------------------------------------------------
+
+local do_atomic_op_with_file = function(filename, action)
+  arguments(
+      "string",   filename,
+      "function", action
+    )
+
+  local res, err
+
+  local file, err = io.open(filename, "a+")
+  if not file then
+    return nil, "do_atomic_op_with_file, open fails: " .. err
+  end
+
+  -- TODO: Very unsafe? Endless loop may occur?
+  while not lfs.lock(file, "w") do end
+
+  -- TODO: Do xpcall() instead of pcall()?
+  local status
+  status, res, err = pcall(action, file)
+  if not status or not res then
+    lfs.unlock(file)
+    if not status then
+      err = res
+    end
+    return nil, "do_atomic_op_with_file, pcall fails: " .. err
+  end
+
+  res, err = lfs.unlock(file)
+  if not res then
+    file:close()
+    file = nil
+    return nil, "do_atomic_op_with_file, unlock fails: " .. err
+  end
+
+  file:close()
+  file = nil
+  return true
+end
+
+--------------------------------------------------------------------------------
+
+return
+{
+  find_all_files = find_all_files;
+  write_file = write_file;
+  read_file = read_file;
+  update_file = update_file;
+  create_path_to_file = create_path_to_file;
+  do_atomic_op_with_file = do_atomic_op_with_file;
+}
---
+++ test/cases/0001-filesystem.lua
@@ -0,0 +1,167 @@
+--------------------------------------------------------------------------------
+-- 0001-filesystem.lua: tests for file system
+--------------------------------------------------------------------------------
+
+local make_suite = assert(loadfile('test-lib/init.lua')((...)))
+
+--------------------------------------------------------------------------------
+
+local socket = require 'socket'
+
+--------------------------------------------------------------------------------
+
+local arguments,
+      method_arguments,
+      optional_arguments
+      = import 'lua-nucleo/args.lua'
+      {
+        'arguments',
+        'method_arguments',
+        'optional_arguments'
+      }
+
+local ensure,
+      ensure_equals,
+      ensure_strequals,
+      ensure_tequals,
+      ensure_error,
+      ensure_aposteriori_probability,
+      ensure_fails_with_substring
+      = import 'lua-nucleo/ensure.lua'
+      {
+        'ensure',
+        'ensure_equals',
+        'ensure_strequals',
+        'ensure_tequals',
+        'ensure_error',
+        'ensure_aposteriori_probability',
+        'ensure_fails_with_substring'
+      }
+
+local assert_is_string
+      = import 'lua-nucleo/typeassert.lua'
+      {
+        'assert_is_string'
+      }
+
+-- TODO: Should use smth like fork_processes()
+local do_with_servers
+      = import 'test-lib/server.lua'
+      {
+        'do_with_servers'
+      }
+
+local find_all_files,
+      write_file,
+      read_file,
+      update_file,
+      create_path_to_file,
+      do_atomic_op_with_file,
+      exports
+      = import 'lua-aplicado/filesystem.lua'
+      {
+        'find_all_files',
+        'write_file',
+        'read_file',
+        'update_file',
+        'create_path_to_file',
+        'do_atomic_op_with_file'
+      }
+
+
+--------------------------------------------------------------------------------
+
+local test = make_suite("filesystem", exports)
+
+--------------------------------------------------------------------------------
+
+local register_temp_file
+do
+  local files_to_be_removed = {}
+
+  register_temp_file = function(filename)
+    os.remove(filename)
+    files_to_be_removed[#files_to_be_removed + 1] = filename
+    return filename
+  end
+
+  test:tear_down(function()
+    for i = 1, #files_to_be_removed do
+      os.remove(files_to_be_removed[i])
+    end
+  end)
+end
+
+--------------------------------------------------------------------------------
+
+test:group 'do_atomic_op_with_file'
+
+--------------------------------------------------------------------------------
+
+test "update-file-from-2-threads" (function()
+  local TEST_FILE_DO_ATOMIC = register_temp_file("./data/test_do_atomic")
+
+  local make_callback = function(op_timeout, data)
+    arguments(
+      "number", op_timeout,
+      "string", data
+      )
+
+    return function(file)
+      socket.sleep(op_timeout)
+      return file:write(data)
+    end
+  end
+
+  local make_file_updater = function(start_timeout, op_timeout, data)
+    arguments(
+      "number", start_timeout,
+      "number", op_timeout,
+      "string", data
+      )
+
+    return function()
+      socket.sleep(start_timeout)
+      local res, err = do_atomic_op_with_file(TEST_FILE_DO_ATOMIC, make_callback(op_timeout, data))
+      if not res then
+        error("worker `" .. data .. "' failed: " .. err)
+      end
+    end
+  end
+
+  local make_file_updater_loops = function()
+    local workers = {}
+
+    workers[#workers + 1] = make_file_updater(0,  2, "(worker 1)")
+    workers[#workers + 1] = make_file_updater(1,  0, "(worker 2)")
+
+    return workers
+  end
+
+  local expected = "(worker 1)(worker 2)"
+
+  do_with_servers(
+      make_file_updater_loops(),
+      function(pid_list)
+        socket.sleep(3)
+        local actual = read_file(TEST_FILE_DO_ATOMIC)
+        ensure_strequals("file content after atomic ops", actual, expected)
+      end
+    )
+end)
+
+--------------------------------------------------------------------------------
+
+test:UNTESTED "find_all_files"
+test:UNTESTED "write_file"
+test:UNTESTED "read_file"
+test:UNTESTED "update_file"
+test:UNTESTED "create_path_to_file"
+
+--------------------------------------------------------------------------------
+
+-- TODO: Add more tests on do_atomic_op_with_file()
+
+--------------------------------------------------------------------------------
+
+assert(test:run())
