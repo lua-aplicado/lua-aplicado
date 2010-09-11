502e800bbc20d107fc5c32179a45a3a0fbb0ec34
Alexander Gladysh <agladysh@gmail.com>
2010-09-12 01:08:33 +0400

    lua-aplicado: module
    To allow initialization with `require 'lua-aplicado.module'`

---
+++ lua-aplicado/module.lua
@@ -0,0 +1,7 @@
+--------------------------------------------------------------------------------
+-- module.lua: lua-aplicado module bootstrapper
+--------------------------------------------------------------------------------
+
+require 'lua-nucleo.module'
+
+-- You may also want to require 'lua-nucleo.strict'.
