265a2444e37bdd4412bddaa20e56d81eca7ab62c
Alexander Gladysh <agladysh@gmail.com>
2010-12-22 07:28:16 +0300

    code/globals: lists of legitimate system globals

---
+++ lua-aplicado/code/globals/lua-nucleo.lua
@@ -0,0 +1,21 @@
+--------------------------------------------------------------------------------
+-- lua-nucleo.lua: list of globals defined in lua-nucleo
+--------------------------------------------------------------------------------
+
+-- TODO: Ensure this list is complete.
+-- TODO: Generate by filtering exports/declare statements
+
+local LUA_NUCLEO_GLOBALS =
+{
+  "exports";
+  "declare";
+  "is_declared";
+  "import";
+}
+
+--------------------------------------------------------------------------------
+
+return
+{
+  LUA_NUCLEO_GLOBALS = LUA_NUCLEO_GLOBALS;
+}
