02a5d4e1845752f9e1e60b7b33a8a2e30571a1a7
Alexander Gladysh <agladysh@gmail.com>
2010-12-22 06:02:35 +0300

    rockspec/pk-rocks-manifest: fixed

--- rockspec/pk-rocks-manifest.lua
+++ rockspec/pk-rocks-manifest.lua
@@ -8,7 +8,7 @@ local ROCKS =
     "rockspec/lua-aplicado-scm-1.rockspec";
     generator =
     {
-      "pk-lua-interpreter", "etc/rockspec/generate.lua", "banner-1",
+      "pk-lua-interpreter", "etc/rockspec/generate.lua", "scm-1",
         ">", "rockspec/lua-aplicado-scm-1.rockspec"
     };
   };
