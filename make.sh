d27e2c2f1e8eb8db96f7819c09dddc76a5a5114f
Vladimir Fedin <vl.fedin@gmail.com>
2011-05-11 03:04:01 +0400

    make.sh: clean-up and fixes

--- make.sh
+++ make.sh
@@ -3,15 +3,13 @@
 set -e
 
 echo "----> Making manifest"
-sudo luajit2 etc/rockspec/generate.lua banner-1 > rockspec/lua-aplicado-banner-1.rockspec
+luajit2 etc/rockspec/generate.lua banner-1 > rockspec/lua-aplicado-banner-1.rockspec
+
+echo "----> Creating list-exports"
+etc/list-exports/list-exports list_all
 
 echo "----> Making rocks"
 sudo luarocks make rockspec/lua-aplicado-banner-1.rockspec
-
-echo "----> Creating list-exports"
-cd etc/list-exports/
-sudo ./list-exports --root=../.. list_all
-cd ../..
 
 echo "----> Restarting multiwatch and LJ2"
 sudo killall multiwatch && sudo killall luajit2
