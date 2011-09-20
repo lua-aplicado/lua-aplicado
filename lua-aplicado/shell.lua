f4af45ab1964e3befc1ed37d25f2635e0ffda81e
Alexander V. Nikolaev <avn@daemon.hole.ru>
2011-09-20 22:58:42 +0000

    Better shell_read, raise error when command fails

--- lua-aplicado/shell.lua
+++ lua-aplicado/shell.lua
@@ -2,6 +2,8 @@
 -- shell.lua: dumb ad-hoc code to work with sh-like shell
 --------------------------------------------------------------------------------
 
+require 'posix'
+
 local assert, error, tostring
     = assert, error, tostring
 
@@ -9,7 +11,10 @@ local table_concat = table.concat
 
 local os_execute = os.execute
 
-local io_popen = io.popen
+local posix_fork, posix_exec, posix_pipe, posix_dup2, posix_wait, posix_close,
+      posix_open, posix_read, posix_write, posix_WNOHANG, posix_BUFSIZ
+    = posix.fork, posix.exec, posix.pipe, posix.dup2, posix.wait, posix.close,
+      posix.open, posix.read, posix.write, posix.WNOHANG, posix.BUFSIZ
 
 --------------------------------------------------------------------------------
 
@@ -30,6 +35,12 @@ local is_number
       }
 
 local tset = import 'lua-nucleo/table-utils.lua' { 'tset' }
+
+local make_concatter
+      = import 'lua-nucleo/string.lua'
+      {
+        'make_concatter'
+      }
 
 --------------------------------------------------------------------------------
 
@@ -126,26 +137,81 @@ local shell_exec_no_subst = function(...)
   return assert(os_execute(cmd))
 end
 
+-- Alternative io.popen, able to catch errors from subprocesses
+-- Not compatible with popen, return blob with all output
+-- Also, have different behavior with io.popen in lua 5.2, by closing stdin
+-- of child process
+local shell_read_popen = function(cmd)
+  local rpipe, wpipe = posix_pipe()
+  if rpipe == nil then
+    local err = wpipe
+    error("can't create pipe: " .. err)
+  end
+
+  local pid = posix_fork()
+  if pid < 0 then
+    error("can't fork")
+  end
+  if pid > 0 then
+    -- in parent:
+    -- close fd of "remote" side of pipe, we do not use it
+    posix_close(wpipe)
+    local cat, concat = make_concatter()
+    while  true do
+      local buf = posix_read(rpipe, posix_BUFSIZ)
+      if buf == nil or #buf == 0 then
+        break
+      end
+      cat(buf)
+    end
+
+    -- close our side of pipe
+    posix_close(rpipe)
+
+    -- wait child process, and check rc
+    -- if wpid not equal pid then raise error
+
+    local wpid, status, rc = posix_wait(pid)
+    if wpid == pid  then
+      if rc > 0 then
+        error("command `" .. cmd .. "' stopped with rc==" .. rc)
+      end
+    else
+      error("can't waid for `" .. cmd .. "' [" .. pid .. "]")
+    end
+    return concat()
+  else
+    -- in child process:
+    local STDIN_FILENO = 0
+    local STDOUT_FILENO = 1
+    local STDERR_FILENO = 2
+
+    -- force stdin = /dev/null
+    local dev_null = posix_open("/dev/null", {}, "r")
+    posix_dup2(dev_null, STDIN_FILENO)
+    posix_close(dev_null)
+
+    -- close "remote" side of pipe, and attach pipe to stdout
+    posix_close(rpipe)
+    posix_dup2(wpipe, STDOUT_FILENO)
+    posix_close(wpipe)
+
+    -- TODO: should explicitly close all descriptors, except 0,1,2
+    -- GH#1 -- https://github.com/lua-aplicado/lua-aplicado/issues/1
+    err, msg = posix_exec("/bin/sh", "-c", cmd)
+
+    -- we can't raise exceptions here
+    posix.write(STDERR_FILENO, "can't exec:" .. (msg or "unreachable") .. "\n")
+    os.exit(1)
+  end
+end
+
 local shell_read = function(...)
-  local cmd = shell_format_command(...)
-  -- print("reading:", cmd)
-  local f = assert(io_popen(cmd))
-  local result = f:read("*a")
-  f:close()
-  f = nil
-  -- print("READ", "`"..result.."'")
-  return result
+  return shell_read_popen(shell_format_command(...))
 end
 
 local shell_read_no_subst = function(...)
-  local cmd = shell_format_command_no_subst(...)
-  -- print("reading:", cmd)
-  local f = assert(io_popen(cmd))
-  local result = f:read("*a")
-  f:close()
-  f = nil
-  -- print("READ", "`"..result.."'")
-  return result
+  return shell_read_popen(shell_format_command_no_subst(...))
 end
 
 --------------------------------------------------------------------------------
