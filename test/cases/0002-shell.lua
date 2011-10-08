7785b5e87823b4110e7ccf40fc91aa6a8c8accaa
Alexander V. Nikolaev <avn@daemon.hole.ru>
2011-10-08 00:21:54 +0000

    Test for shell_read

---
+++ test/cases/0002-shell.lua
@@ -0,0 +1,58 @@
+--------------------------------------------------------------------------------
+-- 0002-shell-read.lua: tests for shell piping
+--------------------------------------------------------------------------------
+
+local make_suite = ...
+
+--------------------------------------------------------------------------------
+
+local shell_read,
+      exports
+      = import 'lua-aplicado/shell.lua'
+      {
+        'shell_read'
+      }
+
+local ensure,
+      ensure_equals,
+      ensure_strequals,
+      ensure_error,
+      ensure_fails_with_substring
+      = import 'lua-nucleo/ensure.lua'
+      {
+        'ensure',
+        'ensure_equals',
+        'ensure_strequals',
+        'ensure_error',
+        'ensure_fails_with_substring'
+      }
+
+local test = make_suite("shell", exports)
+
+test:test_for "shell_read" (function()
+    ensure_strequals(
+        "plain read",
+        shell_read("/bin/echo", "foobar"),
+        "foobar\n"
+      )
+    ensure_equals("empty read", shell_read("/bin/true"), "")
+    ensure_fails_with_substring(
+        "false read",
+        (function()
+            shell_read("/bin/false")
+        end),
+        "command `/bin/false' stopped with rc==1"
+      )
+end)
+
+test:UNTESTED "shell_format_command_no_subst"
+test:UNTESTED "shell_escape_many"
+test:UNTESTED "shell_escape_no_subst"
+test:UNTESTED "shell_exec_no_subst"
+test:UNTESTED "shell_escape"
+test:UNTESTED "shell_format_command"
+test:UNTESTED "shell_exec"
+test:UNTESTED "shell_read_no_subst"
+test:UNTESTED "shell_escape_many_no_subst"
+
+test:run()
