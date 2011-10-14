d879e179c98e9dcceac442a2992ee4991b1940e1
Oksana Kozlova <oksana.renez@gmail.com>
2011-10-14 06:51:52 +0400

    lua-aplicado/random.lua: random seed generation added, after review

---
+++ lua-aplicado/random.lua
@@ -0,0 +1,33 @@
+--------------------------------------------------------------------------------
+-- random.lua: utilities for random generation
+--------------------------------------------------------------------------------
+
+require 'md5'
+
+local arguments,
+      optional_arguments,
+      method_arguments
+      = import 'lua-nucleo/args.lua'
+      {
+        'arguments',
+        'optional_arguments',
+        'method_arguments'
+      }
+
+--------------------------------------------------------------------------------
+
+-- generates pseudorandom number based on input string
+local random_seed_from_string = function(base_string)
+  base_string = base_string or tostring(os.time())
+  arguments(
+      "string", base_string
+    )
+  return tonumber(string.sub(md5.sumhexa(base_string), 1, 9), 16)
+end
+
+--------------------------------------------------------------------------------
+
+return
+{
+  random_seed_from_string = random_seed_from_string;
+}
