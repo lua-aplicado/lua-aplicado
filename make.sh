6cfffd7f73d9351c0343c59133483859536838f5
Mikhail Nazarov <nazarov.mihail@gmail.com>
2011-12-16 21:30:38 +0400

    make scm-1 version in make.sh

--- make.sh
+++ make.sh
@@ -7,11 +7,12 @@ etc/list-exports/list-exports list_all
 
 echo "----> Generating rockspecs"
 lua etc/rockspec/generate.lua banner-1 > rockspec/lua-aplicado-banner-1.rockspec
-
 lua etc/rockspec/generate.lua scm-1 > rockspec/lua-aplicado-scm-1.rockspec
 
+echo "----> Remove a rock"
+sudo luarocks remove --force lua-aplicado
 echo "----> Making rocks"
-sudo luarocks make rockspec/lua-aplicado-banner-1.rockspec
+sudo luarocks make rockspec/lua-aplicado-scm-1.rockspec
 
 echo "----> Restarting multiwatch and LJ2"
 sudo killall multiwatch && sudo killall luajit2
