991673c315b207f8b6897caf29907f8dc64360f9
Mikhail Nazarov <nazarov.mihail@gmail.com>
2011-12-16 21:31:08 +0400

    remove banner-1 rockspec

--- make.sh
+++ make.sh
@@ -6,7 +6,6 @@ echo "----> Creating list-exports"
 etc/list-exports/list-exports list_all
 
 echo "----> Generating rockspecs"
-lua etc/rockspec/generate.lua banner-1 > rockspec/lua-aplicado-banner-1.rockspec
 lua etc/rockspec/generate.lua scm-1 > rockspec/lua-aplicado-scm-1.rockspec
 
 echo "----> Remove a rock"
