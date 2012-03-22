41bc169e6a32c4169f8c5cd3242b8147784ae7a6
Valeriy Skurikhin <Keen.vs@gmail.com>
2012-03-22 18:07:41 +0400

    Fixed indentation

--- test/cases/0002-shell.lua
+++ test/cases/0002-shell.lua
@@ -59,19 +59,19 @@ local test = make_suite("shell", exports)
 --------------------------------------------------------------------------------
 
 test:test_for "shell_read" (function()
-    ensure_strequals(
-        "plain read",
-        shell_read("/bin/echo", "foobar"),
-        "foobar\n"
-      )
-    ensure_equals("empty read", shell_read("/bin/true"), "")
-    ensure_fails_with_substring(
-        "false read",
-        (function()
-            shell_read("/bin/false")
-        end),
-        "command `/bin/false' stopped with rc==1"
-      )
+  ensure_strequals(
+      "plain read",
+      shell_read("/bin/echo", "foobar foo"),
+      "foobar foo\n"
+    )
+  ensure_equals("empty read", shell_read("/bin/true"), "")
+  ensure_fails_with_substring(
+      "false read",
+      (function()
+          shell_read("/bin/false")
+      end),
+      "command `/bin/false' stopped with rc==1"
+    )
 end)
 
 --------------------------------------------------------------------------------
@@ -100,21 +100,21 @@ end)
 test:case "various_exit_codes" (function ()
   for code = 1, 2 do
     ensure_fails_with_substring(
-      "failed rc "..code,
-      (function()
-        shell_write("exit " .. code .. "\n", "/bin/sh")
-      end),
-     "command `/bin/sh' stopped with rc=="..code
-    )
+        "failed rc "..code,
+        (function()
+          shell_write("exit '" .. code .. "'\n", "/bin/sh")
+        end),
+        "command `/bin/sh' stopped with rc=="..code
+      )
   end
 end)
 
 --------------------------------------------------------------------------------
 
 test:test_for "shell_exec" (function ()
-    ensure_equals("/bin/true", 0, shell_exec("/bin/true"))
-    local rc = shell_exec("/bin/false")
-    ensure("/bin/false", rc ~= 0)
+  ensure_equals("/bin/true", 0, shell_exec("/bin/true"))
+  local rc = shell_exec("/bin/false")
+  ensure("/bin/false", rc ~= 0)
 end)
 
 --------------------------------------------------------------------------------
