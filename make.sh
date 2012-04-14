63566e43fe6204b0fb5982c4b1cd1b2087b61cda
Vladimir Fedin <vl.fedin@gmail.com>
2012-04-14 15:07:05 +0400

    make.sh: fix

--- make.sh
+++ make.sh
@@ -9,11 +9,11 @@ echo "----> Generating rockspecs"
 lua etc/rockspec/generate.lua scm-1 > rockspec/lua-aplicado-scm-1.rockspec
 
 echo "----> Remove a rock"
-sudo luarocks remove --force lua-aplicado
+sudo luarocks remove --force lua-aplicado || true
 echo "----> Making rocks"
 sudo luarocks make rockspec/lua-aplicado-scm-1.rockspec
 
 echo "----> Restarting multiwatch and LJ2"
-sudo killall multiwatch && sudo killall luajit2
+sudo killall multiwatch || true ; sudo killall luajit2 || true
 
 echo "----> OK"
