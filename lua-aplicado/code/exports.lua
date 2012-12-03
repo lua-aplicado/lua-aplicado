0e458bb7fd9a783f1202f536acae9032b566e36a
Alexey Romanov <jobmail37@gmail.com>
2012-12-03 13:01:13 +0400

    regenerated all

--- lua-aplicado/code/exports.lua
+++ lua-aplicado/code/exports.lua
@@ -10,111 +10,444 @@
 
 return
 {
-  LOM_ATTRS = { "lua-aplicado/expat.lua" };
-  call = { "lua-aplicado/error.lua" };
-  common_init_logging_to_file = { "lua-aplicado/log.lua" };
-  copy_file = { "lua-aplicado/shell/filesystem.lua" };
-  copy_file_to_dir = { "lua-aplicado/shell/filesystem.lua" };
-  copy_file_with_flag = { "lua-aplicado/shell/filesystem.lua" };
-  create_common_logging_system = { "lua-aplicado/log.lua" };
-  create_email = { "lua-aplicado/shell/send_email.lua" };
-  create_error_object = { "lua-aplicado/error.lua" };
-  create_path_to_file = { "lua-aplicado/filesystem.lua" };
-  create_symlink_from_to = { "lua-aplicado/shell/filesystem.lua" };
-  create_temporary_directory = { "lua-aplicado/filesystem.lua" };
-  csv_convert_to_table = { "lua-aplicado/csv.lua" };
-  do_atomic_op_with_file = { "lua-aplicado/filesystem.lua" };
-  does_file_exist = { "lua-aplicado/filesystem.lua" };
-  error_handler_for_call = { "lua-aplicado/error.lua" };
-  fail = { "lua-aplicado/error.lua" };
-  find_all_files = { "lua-aplicado/filesystem.lua" };
-  get_common_logging_system = { "lua-aplicado/log.lua" };
-  get_current_logsystem_date_microsecond = { "lua-aplicado/log.lua" };
-  get_extension = { "lua-aplicado/filesystem.lua" };
-  get_filename_from_path = { "lua-aplicado/filesystem.lua" };
-  git_add_directory = { "lua-aplicado/shell/git.lua" };
-  git_are_branches_different = { "lua-aplicado/shell/git.lua" };
-  git_commit_with_editable_message = { "lua-aplicado/shell/git.lua" };
-  git_commit_with_message = { "lua-aplicado/shell/git.lua" };
-  git_config_get_remote_url = { "lua-aplicado/shell/git.lua" };
-  git_exec = { "lua-aplicado/shell/git.lua" };
-  git_format_command = { "lua-aplicado/shell/git.lua" };
-  git_get_tracking_branch_name_of_HEAD = { "lua-aplicado/shell/git.lua" };
-  git_has_untracked_files = { "lua-aplicado/shell/git.lua" };
-  git_init_subtree = { "lua-aplicado/shell/git.lua" };
-  git_is_directory_dirty = { "lua-aplicado/shell/git.lua" };
-  git_is_dirty = { "lua-aplicado/shell/git.lua" };
-  git_is_file_changed_between_revisions = { "lua-aplicado/shell/git.lua" };
-  git_load_config = { "lua-aplicado/shell/git.lua" };
-  git_pull_subtree = { "lua-aplicado/shell/git.lua" };
-  git_push_all = { "lua-aplicado/shell/git.lua" };
-  git_read = { "lua-aplicado/shell/git.lua" };
-  git_remote_add = { "lua-aplicado/shell/git.lua" };
-  git_remote_rm = { "lua-aplicado/shell/git.lua" };
-  git_update_index = { "lua-aplicado/shell/git.lua" };
-  is_common_logging_system_initialized = { "lua-aplicado/log.lua" };
-  is_directory = { "lua-aplicado/filesystem.lua" };
-  is_error_object = { "lua-aplicado/error.lua" };
-  join_path = { "lua-aplicado/filesystem.lua" };
-  load_all_files = { "lua-aplicado/filesystem.lua" };
-  load_all_files_with_curly_placeholders = { "lua-aplicado/filesystem.lua" };
-  luarocks_admin_exec_dir = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_admin_make_manifest = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_ensure_rock_not_installed_forced = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_exec = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_exec_dir = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_exec_dir_no_sudo = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_exec_no_sudo = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_get_rocknames_in_manifest = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_install_from = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_list_rockspec_files = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_load_manifest = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_load_rockspec = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_make_in = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_pack_to = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_parse_installed_rocks = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_read = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_remove_forced = { "lua-aplicado/shell/luarocks.lua" };
-  luarocks_show_rock_dir = { "lua-aplicado/shell/luarocks.lua" };
-  make_chunk_inspector = { "lua-aplicado/chunk_inspector.lua" };
-  make_cookie_jar = { "lua-aplicado/web/cookie_jar.lua" };
-  make_loggers = { "lua-aplicado/log.lua" };
-  normalize_path = { "lua-aplicado/filesystem.lua" };
-  random_seed_from_string = { "lua-aplicado/random.lua" };
-  read_file = { "lua-aplicado/filesystem.lua" };
-  remote_luarocks_ensure_rock_not_installed_forced = { "lua-aplicado/shell/remote_luarocks.lua" };
-  remote_luarocks_install_from = { "lua-aplicado/shell/remote_luarocks.lua" };
-  remote_luarocks_list_installed_rocks = { "lua-aplicado/shell/remote_luarocks.lua" };
-  remote_luarocks_remove_forced = { "lua-aplicado/shell/remote_luarocks.lua" };
-  remove_file = { "lua-aplicado/shell/filesystem.lua" };
-  remove_recursively = { "lua-aplicado/shell/filesystem.lua" };
-  rethrow = { "lua-aplicado/error.lua" };
-  rm_tree = { "lua-aplicado/filesystem.lua" };
-  running_under_luajit = { "lua-aplicado/luajit2.lua" };
-  send_email = { "lua-aplicado/shell/send_email.lua" };
-  shell_escape = { "lua-aplicado/shell.lua" };
-  shell_escape_many = { "lua-aplicado/shell.lua" };
-  shell_escape_many_no_subst = { "lua-aplicado/shell.lua" };
-  shell_escape_no_subst = { "lua-aplicado/shell.lua" };
-  shell_exec = { "lua-aplicado/shell.lua" };
-  shell_exec_no_subst = { "lua-aplicado/shell.lua" };
-  shell_exec_remote = { "lua-aplicado/shell/remote.lua" };
-  shell_format_command = { "lua-aplicado/shell.lua" };
-  shell_format_command_no_subst = { "lua-aplicado/shell.lua" };
-  shell_read = { "lua-aplicado/shell.lua" };
-  shell_read_no_subst = { "lua-aplicado/shell.lua" };
-  shell_read_remote = { "lua-aplicado/shell/remote.lua" };
-  shell_wait = { "lua-aplicado/shell.lua" };
-  shell_write = { "lua-aplicado/shell.lua" };
-  shell_write_async = { "lua-aplicado/shell.lua" };
-  shell_write_async_no_subst = { "lua-aplicado/shell.lua" };
-  shell_write_no_subst = { "lua-aplicado/shell.lua" };
-  splitpath = { "lua-aplicado/filesystem.lua" };
-  temporary_directory = { "lua-aplicado/testing/decorators.lua" };
-  try = { "lua-aplicado/error.lua" };
-  update_file = { "lua-aplicado/filesystem.lua" };
-  write_file = { "lua-aplicado/filesystem.lua" };
-  xcall = { "lua-aplicado/error.lua" };
-  xfinally = { "lua-aplicado/error.lua" };
-  xml_convert_lom = { "lua-aplicado/expat.lua" };
+  LOM_ATTRS =
+  {
+    { "lua-aplicado/expat.lua", w = 0 };
+  };
+  call =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  common_init_logging_to_file =
+  {
+    { "lua-aplicado/log.lua", w = 0 };
+  };
+  copy_file =
+  {
+    { "lua-aplicado/shell/filesystem.lua", w = 0 };
+  };
+  copy_file_to_dir =
+  {
+    { "lua-aplicado/shell/filesystem.lua", w = 0 };
+  };
+  copy_file_with_flag =
+  {
+    { "lua-aplicado/shell/filesystem.lua", w = 0 };
+  };
+  create_common_logging_system =
+  {
+    { "lua-aplicado/log.lua", w = 0 };
+  };
+  create_email =
+  {
+    { "lua-aplicado/shell/send_email.lua", w = 0 };
+  };
+  create_error_object =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  create_path_to_file =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  create_symlink_from_to =
+  {
+    { "lua-aplicado/shell/filesystem.lua", w = 0 };
+  };
+  create_temporary_directory =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  csv_convert_to_table =
+  {
+    { "lua-aplicado/csv.lua", w = 0 };
+  };
+  do_atomic_op_with_file =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  does_file_exist =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  error_handler_for_call =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  fail =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  find_all_files =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  get_common_logging_system =
+  {
+    { "lua-aplicado/log.lua", w = 0 };
+  };
+  get_current_logsystem_date_microsecond =
+  {
+    { "lua-aplicado/log.lua", w = 0 };
+  };
+  get_extension =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  get_filename_from_path =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  git_add_directory =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_are_branches_different =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_clone =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_commit_with_editable_message =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_commit_with_message =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_config_get_remote_url =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_exec =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_format_command =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_get_tracking_branch_name_of_HEAD =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_has_untracked_files =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_init =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_init_bare =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_init_subtree =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_is_directory_dirty =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_is_dirty =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_is_file_changed_between_revisions =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_load_config =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_pull_subtree =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_push_all =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_read =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_remote_add =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_remote_rm =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  git_update_index =
+  {
+    { "lua-aplicado/shell/git.lua", w = 0 };
+  };
+  is_common_logging_system_initialized =
+  {
+    { "lua-aplicado/log.lua", w = 0 };
+  };
+  is_directory =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  is_error_object =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  join_path =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  load_all_files =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  load_all_files_with_curly_placeholders =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  luarocks_admin_exec_dir =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_admin_make_manifest =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_ensure_rock_not_installed_forced =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_exec =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_exec_dir =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_exec_dir_no_sudo =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_exec_no_sudo =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_get_rocknames_in_manifest =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_install_from =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_list_rockspec_files =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_load_manifest =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_load_rockspec =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_make_in =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_pack_to =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_parse_installed_rocks =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_read =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_remove_forced =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  luarocks_show_rock_dir =
+  {
+    { "lua-aplicado/shell/luarocks.lua", w = 0 };
+  };
+  make_chunk_inspector =
+  {
+    { "lua-aplicado/chunk_inspector.lua", w = 0 };
+  };
+  make_cookie_jar =
+  {
+    { "lua-aplicado/web/cookie_jar.lua", w = 0 };
+  };
+  make_loggers =
+  {
+    { "lua-aplicado/log.lua", w = 0 };
+  };
+  normalize_path =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  random_seed_from_string =
+  {
+    { "lua-aplicado/random.lua", w = 0 };
+  };
+  read_file =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  remote_luarocks_ensure_rock_not_installed_forced =
+  {
+    { "lua-aplicado/shell/remote_luarocks.lua", w = 0 };
+  };
+  remote_luarocks_install_from =
+  {
+    { "lua-aplicado/shell/remote_luarocks.lua", w = 0 };
+  };
+  remote_luarocks_list_installed_rocks =
+  {
+    { "lua-aplicado/shell/remote_luarocks.lua", w = 0 };
+  };
+  remote_luarocks_remove_forced =
+  {
+    { "lua-aplicado/shell/remote_luarocks.lua", w = 0 };
+  };
+  remove_file =
+  {
+    { "lua-aplicado/shell/filesystem.lua", w = 0 };
+  };
+  remove_recursively =
+  {
+    { "lua-aplicado/shell/filesystem.lua", w = 0 };
+  };
+  rethrow =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  rm_tree =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  running_under_luajit =
+  {
+    { "lua-aplicado/luajit2.lua", w = 0 };
+  };
+  send_email =
+  {
+    { "lua-aplicado/shell/send_email.lua", w = 0 };
+  };
+  shell_escape =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_escape_many =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_escape_many_no_subst =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_escape_no_subst =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_exec =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_exec_no_subst =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_exec_remote =
+  {
+    { "lua-aplicado/shell/remote.lua", w = 0 };
+  };
+  shell_format_command =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_format_command_no_subst =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_read =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_read_no_subst =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_read_remote =
+  {
+    { "lua-aplicado/shell/remote.lua", w = 0 };
+  };
+  shell_wait =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_write =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_write_async =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_write_async_no_subst =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  shell_write_no_subst =
+  {
+    { "lua-aplicado/shell.lua", w = 0 };
+  };
+  splitpath =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  temporary_directory =
+  {
+    { "lua-aplicado/testing/decorators.lua", w = 0 };
+  };
+  try =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  update_file =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  write_file =
+  {
+    { "lua-aplicado/filesystem.lua", w = 0 };
+  };
+  xcall =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  xfinally =
+  {
+    { "lua-aplicado/error.lua", w = 0 };
+  };
+  xml_convert_lom =
+  {
+    { "lua-aplicado/expat.lua", w = 0 };
+  };
 }
