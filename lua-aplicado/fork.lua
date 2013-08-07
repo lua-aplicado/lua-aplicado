37c638429ffd6afd7fbf34b5303cbc3054a99f1a
Mark Gurevich <markgurevichster@gmail.com>
2013-08-07 11:32:33 +0400

    lua-aplicado/fork: Implement fork() and atfork() with tests

---
+++ lua-aplicado/fork.lua
@@ -0,0 +1,70 @@
+--------------------------------------------------------------------------------
+-- fork.lua: wrapper around posix.fork()
+-- This file is a part of Lua-Aplicado library
+-- Copyright (c) Lua-Aplicado authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+local posix = require 'posix'
+
+--------------------------------------------------------------------------------
+
+local optional_arguments
+      = import 'lua-nucleo/args.lua'
+      {
+        'optional_arguments',
+      }
+
+--------------------------------------------------------------------------------
+
+local fork
+local atfork
+do
+  local prepare_callbacks = {}
+  local parent_callbacks = {}
+  local child_callbacks = {}
+
+  --- Call posix.fork() and all registered callbacks
+  -- @return Returns whatever posix.fork() returns
+  -- @see http://luaposix.github.io/luaposix/docs/index.html#fork
+  fork = function()
+    for i = 1, #prepare_callbacks do
+      prepare_callbacks[i]()
+    end
+    local cpid = posix.fork()
+    if cpid > 0 then
+      for j = 1, #parent_callbacks do
+        parent_callbacks[j]()
+      end
+    else
+      for k = 1, #child_callbacks do
+        child_callbacks[k]()
+      end
+    end
+    return cpid
+  end
+
+  --- Register callbacks to be executed when fork() is called
+  -- @param on_prepare (optional) Callback to be called before fork()
+  -- @param on_parent (optional) Callback to be called after fork() in parent
+  -- process
+  -- @param on_child (optional) Callback to be called after fork() in child
+  -- process
+  atfork = function(on_prepare, on_parent, on_child)
+    optional_arguments(
+        "function", on_prepare,
+        "function", on_parent,
+        "function", on_child
+      )
+    prepare_callbacks[#prepare_callbacks + 1] = on_prepare
+    parent_callbacks[#parent_callbacks + 1] = on_parent
+    child_callbacks[#child_callbacks + 1] = on_child
+  end
+end
+
+--------------------------------------------------------------------------------
+
+return
+{
+  fork = fork;
+  atfork = atfork;
+}
---
+++ test/cases/0070-fork.lua
@@ -0,0 +1,66 @@
+--------------------------------------------------------------------------------
+-- test/cases/0070-fork.lua: tests for fork.lua
+-- This file is a part of Lua-Aplicado library
+-- Copyright (c) Lua-Aplicado authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+local posix = require 'posix'
+
+local ensure,
+      ensure_equals
+      = import 'lua-nucleo/ensure.lua'
+      {
+        'ensure',
+        'ensure_equals',
+      }
+
+local fork,
+      atfork,
+      exports
+      = import 'lua-aplicado/fork.lua'
+      {
+        'fork',
+        'atfork',
+      }
+
+--------------------------------------------------------------------------------
+
+local log, dbg, spam, log_error
+      = import 'lua-aplicado/log.lua' { 'make_loggers' } (
+          "test/fork", "T070"
+      )
+
+--------------------------------------------------------------------------------
+
+local test = (...)("fork", exports)
+
+--------------------------------------------------------------------------------
+
+test:tests_for "fork"
+test:tests_for "atfork"
+
+test:case "fork-and-atfork-work" (function()
+  local x = ""
+  local on_prepare = function()
+    x = x .. "prepare"
+  end
+  local on_parent = function()
+    x = x .. ",parent"
+  end
+  local on_child = function()
+    x = x .. ",child"
+  end
+  atfork(on_prepare, on_parent, on_child)
+  local cpid = fork()
+  if cpid > 0 then
+    local _, _, status = posix.wait(cpid)
+    ensure_equals("should call right callbacks in child", status, 11)
+    ensure_equals("should call right callbacks in parent", x, "prepare,parent")
+  else
+    if x ~= "prepare,child" then
+      posix._exit(10)
+    else
+      posix._exit(11) -- "These go to eleven"
+    end
+  end
+end)
