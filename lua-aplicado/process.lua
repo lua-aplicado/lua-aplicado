886fee09406d5ecc1ba3bcf35507b0fe93651e80
Alexander V. Nikolaev <avn@daemon.hole.ru>
2013-09-30 15:09:30 +0000

    publish retry_n_times function
    Signed-off-by: Alexander V. Nikolaev <avn@daemon.hole.ru>

---
+++ lua-aplicado/process.lua
@@ -0,0 +1,49 @@
+--------------------------------------------------------------------------------
+-- process.lua: utilities for various process management
+-- This file is a part of Lua-Aplicado library
+-- Copyright (c) Lua-Aplicado authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+local socket = require 'socket'
+
+local arguments,
+      optional_arguments,
+      method_arguments,
+      pack
+      = import 'lua-nucleo/args.lua'
+      {
+        'arguments',
+        'optional_arguments',
+        'method_arguments',
+        'pack'
+      }
+
+--------------------------------------------------------------------------------
+
+local retry_n_times = function(fn, attempts, sleep_time, error_msg)
+  error_msg = error_msg or "timeout"
+
+  arguments(
+      "function", fn,
+      "number", attempts,
+      "number", sleep_time,
+      "string", error_msg
+    )
+
+  for times = 1, attempts do
+    local n, values = pack(fn())
+    if values[1] then
+      return unpack(values)
+    else
+      socket.sleep(sleep_time)
+    end
+  end
+  return nil, error_msg
+end
+
+--------------------------------------------------------------------------------
+
+return
+{
+  retry_n_times = retry_n_times;
+}
