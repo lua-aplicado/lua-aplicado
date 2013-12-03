ddf853d5dc9be57c3e08021a156cdbe096d636c8
Alexander Gladysh <agladysh@gmail.com>
2013-12-03 18:32:36 +0400

    code/exports: regenerated

--- lua-aplicado/code/exports.lua
+++ lua-aplicado/code/exports.lua
@@ -526,6 +526,10 @@ return
   {
     { "lua-aplicado/dsl/config_dsl.lua", w = 0 };
   };
+  walk_lom_data =
+  {
+    { "lua-aplicado/expat.lua", w = 0 };
+  };
   write_file =
   {
     { "lua-aplicado/filesystem.lua", w = 0 };
